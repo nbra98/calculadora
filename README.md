16700b37344d830c04171c7fc7b77753c797a942
Luis Fernando <117687568+nbra98@users.noreply.github.com>
2022-11-23 18:46:50 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+<h1 align="center">Calculadora ➕➖✖️➗</h1>
+
+<h2 align="center">Meu 3º projeto em Desenvolvimento Front-End ​​💻​​</h2><br><br>
+
+<p align="center"><img alt="Print da aplicação" src="./assets/printAplicacao.jpg"></p><br><br>
+
+<h1 align="center">Sobre 📑</h1>
+<h2 align="center">Uma calculadora com funções essenciais para aprimorar meus conhecimentos em CSS e JavaScript. Ela conta ainda com a opção de mudar de tema (botão branco no canto esquerdo superior), aplicando um background de acordo com o que o usuário preferir.</h2><br><br>
+
+<h1 align="center">Tecnologias ​⌨️ ​</h1>
+<h2 align="center">HTML e CSS - JavaScript - Git e GitHub</h2><br><br>
+
+<h1 align="center">Para acessar ​🔗️</h1>
+<h2><center><a href="https://​nbra98.github.io/calculadora/">Clique aqui</a></center></h2><br><br><br><br>
+
+<p align="center"><img alt="user" src="./assets/nbra98.jpg"></p>
---
+++ script.js
@@ -0,0 +1,26 @@
+// Seleção DOM dos 
+let corpo = document.querySelector("body")
+let valor = document.querySelector("#valor")
+let botao = document.querySelectorAll("span")
+// let botoes = document.querySelector(".botoes")
+let botaoAlternarTema = document.querySelector(".botaoAlternarTema")
+
+// Vetor para iniciar operações requisitadas pelo usuário
+for (let i = 0; i < botao.length; i++) {
+    botao[i].addEventListener("click", function() { // Reconhecendo o 'click' do usuário como início das operações
+        if (this.innerHTML == "=") { // Exibir resultado da operação usando a função 'eval()'¹* para autoreconhecer e calcular as operações
+            valor.innerHTML = eval(valor.innerHTML)
+        } else if (this.innerHTML == "LIMPAR") { // Caso o usuário clique em 'LIMPAR', tudo no display será apagado
+            valor.innerHTML = "";
+        } else {
+            valor.innerHTML += this.innerHTML
+        }    
+    })
+}        
+
+// Botão (.div) para alternar entre o tema claro e escuro 
+botaoAlternarTema.onclick = function() {
+    corpo.classList.toggle('temaEscuro')
+}
+
+// A função Eval avalia a expressão da cadeia de caracteres e retorna seu valor. Exemplo: eval("1 + 1") retorna 2. ¹*
